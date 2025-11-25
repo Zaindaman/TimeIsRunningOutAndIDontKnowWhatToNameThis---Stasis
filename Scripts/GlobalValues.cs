0542d73cfed38c1609d937463b06346eac6b2329
That1Planet <andrewtea100@gmail.com>
2025-11-26 12:42:19 +1300

    tesijfk

--- Scripts/GlobalValues.cs
+++ Scripts/GlobalValues.cs
@@ -6,7 +6,7 @@ public partial class GlobalValues : Node
 	public bool isBulletTime { get; set; }
 	public bool isGameOver { get; set; }
 	public bool isGameStart { get; set; }
-	public int[] levelNumber{ get; set{}; }
+	public int[] levelNumber{ get; set; }
 	
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
