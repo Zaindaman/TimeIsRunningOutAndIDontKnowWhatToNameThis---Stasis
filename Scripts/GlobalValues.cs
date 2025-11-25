7d6c435f51eb4f5a31b83210f8a90fbb24cdc0b6
ZainTheMan <abbasizain041@gmail.com>
2025-11-26 10:08:31 +1300

    Adding Global Script

---
+++ Scripts/GlobalValues.cs
@@ -0,0 +1,21 @@
+using Godot;
+using System;
+
+public partial class GlobalValues : Node
+{
+    public bool IsBulletTime { get; set; }
+	public bool IsGameOver { get; set; }
+	public bool IsGameStart { get; set; }
+
+    // Called when the node enters the scene tree for the first time.
+    public override void _Ready()
+	{
+		
+	}
+
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _Process(double delta)
+	{
+		
+	}
+}
