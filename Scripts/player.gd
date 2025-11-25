3f45e176729ba1b97ef254a4eb62f727756b5277
That1Planet <andrewtea100@gmail.com>
2025-11-26 10:04:24 +1300

    gravity

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -1,24 +1,36 @@
 extends CharacterBody2D
 
 const SPEED = 96
-
+const GRAVITY = 500
+const JUMP_FORCE = -200
 
 func _physics_process(delta: float) -> void:
 	var dir = get_input_direction()
-	velocity = dir * SPEED * delta 
+
+	# Horizontal movement
+	velocity.x = dir.x * SPEED
+
+	# Apply gravity
+	if not is_on_floor():
+		velocity.y += GRAVITY * delta
+	else:
+		if Input.is_action_just_pressed("UP"):
+			velocity.y = JUMP_FORCE
+
+	move_and_slide()
 
 func get_input_direction() -> Vector2:
 	var dir := Vector2.ZERO
-	if Input.is_action_pressed("UP"): 
-		dir.y -= 1
-		print("UP")
-	if Input.is_action_pressed("DOWN"): 
-		dir.y += 1
-		print("DOWNwda")
-	if Input.is_action_pressed("LEFT"): 
+
+	if Input.is_action_pressed("LEFT"):
 		dir.x -= 1
-		print("LEFT")
-	if Input.is_action_pressed("RIGHT"): 
+	if Input.is_action_pressed("RIGHT"):
 		dir.x += 1
-		print("RIGHT")
+
+	# remove these if this is a platformer
+	if Input.is_action_pressed("UP"):
+		dir.y -= 1
+	if Input.is_action_pressed("DOWN"):
+		dir.y += 1
+
 	return dir
