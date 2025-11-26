a167150e12cfa9d21188f57ac181946353f866c6
ZainTheMan <abbasizain041@gmail.com>
2025-11-26 14:54:01 +1300

    offset bullet spawn, gitignore improved

--- Scripts/BulletSpawner.cs
+++ Scripts/BulletSpawner.cs
@@ -5,16 +5,20 @@ public partial class BulletSpawner : CharacterBody2D
 {
     [Export]
     public PackedScene Bullet;
-
+    [Export]
+    public float offsetDistance = 100f;
 
 
     private void _on_timer_timeout()
     {
-        GD.Print("Timer timeout!");
-
         Node2D newBullet = Bullet.Instantiate<Node2D>();
 
+        Vector2 spawnPosition = GlobalPosition + new Vector2(MathF.Cos(Rotation), MathF.Sin(Rotation)) * offsetDistance;
+
+        newBullet.Position = spawnPosition;
+
         GetTree().CurrentScene.AddChild(newBullet);
+
 
     }
 
