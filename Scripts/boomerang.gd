a00e349e5340d9dd478d8c4192537b17b6a152ee
Photonyx <headtummy@gmail.com>
2025-11-27 08:55:05 +1300

    spin
    boomer go brrr

--- Scripts/boomerang.gd
+++ Scripts/boomerang.gd
@@ -2,12 +2,13 @@ extends CharacterBody2D
 var centerpos
 var SPEED = 50
 var direction = Vector2.ZERO
+var rotationrate : float
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	centerpos = global_position.x + 85
 	velocity = direction*SPEED
-	
+	rotationrate = 0
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
@@ -15,4 +16,7 @@ func _process(delta: float) -> void:
 		velocity.x -= 12
 	if global_position.x < centerpos:
 		velocity.x += 12
+	if rotationrate <10:
+		rotationrate+= 0.2
+	$Boomerang.rotation_degrees += rotationrate
 	move_and_slide()
