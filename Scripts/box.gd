abcddfa68ab4f87c069568b86a38d1a2d76f62e4
ZainTheMan <abbasizain041@gmail.com>
2025-12-25 16:41:27 +1300

    bugfixes for box and lvl 4
    connected signal for laser and door
    -> moved it higher so player wont hit it
    
    fix box's code so it behaves how it is intended
    -> reduced gravity scale
    -> code now sets linear velocity and gravityscale to 0
    -> Its now using physics process like it should since its dealing with physics

--- Scripts/box.gd
+++ Scripts/box.gd
@@ -2,14 +2,17 @@ extends RigidBody2D
 var isInversion : bool
 
 
-func _process(delta: float) -> void:
+func _physics_process(delta: float) -> void:
 	if isInversion and GlobalValues.isBulletTime:
-		gravity_scale = 0.7
+		gravity_scale = 0.3
 		return
-		gravity_scale = 0.7
-	if GlobalValues.isBulletTime:
+	elif GlobalValues.isBulletTime:
 		gravity_scale = 0
+		linear_velocity = Vector2.ZERO
+
 		return
+	else:
+		gravity_scale = 0.3
 
 
 func _on_area_2d_area_entered(area: Area2D) -> void:
