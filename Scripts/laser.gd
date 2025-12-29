0b87d82e783a23b895610f550b0028b0402a81a3
ZainTheMan <abbasizain041@gmail.com>
2025-12-29 17:39:23 +1300

    Now is based on change in colision rather than hard coded

--- Scripts/laser.gd
+++ Scripts/laser.gd
@@ -1,26 +1,46 @@
 extends Node2D
 
-@onready var ray = $RayCast2D
-@onready var line = $Line2D
+@onready var ray: RayCast2D = $RayCast2D
+@onready var line: Line2D = $Line2D
+
 signal opendoor
 signal closedoor
 signal damage
 signal enemydamage
-func _physics_process(delta):
+
+var was_colliding: bool = false
+var last_collider: Node2D = null
+
+func _physics_process(delta: float) -> void:
 	ray.force_raycast_update()
-	if ray.is_colliding():
-		var hit_pos = ray.get_collision_point()
+	var is_colliding: bool = ray.is_colliding()
+
+	if is_colliding:
+		var hit_pos: Vector2 = ray.get_collision_point()
 		line.points = [Vector2.ZERO, to_local(hit_pos)]
-		var collider = ray.get_collider()
-		if collider.is_in_group("activator"):
-			emit_signal("opendoor")
-		else:
-			emit_signal("closedoor")
-		if collider.is_in_group("player"):
-			emit_signal("damage")
-		if collider.is_in_group("enemy_laser"):
-			emit_signal("enemydamage")
+		var collider: Node2D = ray.get_collider()
+
+		# react on change
+		if not was_colliding or collider != last_collider:
+			if collider.is_in_group("activator"):
+				emit_signal("opendoor")
+			else:
+				emit_signal("closedoor")
+
+			if collider.is_in_group("player"):
+				emit_signal("damage")
+
+			if collider.is_in_group("enemy_laser"):
+				emit_signal("enemydamage")
+
+		last_collider = collider
+
 	else:
 		line.points = [Vector2.ZERO, ray.target_position]
 
-		
+		# Collision just ended
+		if was_colliding:
+			emit_signal("closedoor")
+			last_collider = null
+
+	was_colliding = is_colliding
